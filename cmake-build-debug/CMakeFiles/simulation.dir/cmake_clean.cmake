14ed7d90a4c277102966794a2ac66c6e8346350c
Marcin521 <marcinkondo134@gmail.com>
2020-11-11 09:10:44 +0100

    add porjedt

---
+++ cmake-build-debug/CMakeFiles/simulation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulation.dir/main.cpp.obj"
+  "libsimulation.dll.a"
+  "simulation.exe"
+  "simulation.exe.manifest"
+  "simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
